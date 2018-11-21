2d38886a8379d9a887fa743b5f96102ebce0c542
yuriimchg <yurgan4@gmail.com>
2018-11-21 13:41:23 +0200

    Create postgresql database with sqlalchemy and psycopg2

---
+++ create_db.py
@@ -0,0 +1,18 @@
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy import create_engine
+from configs.app_config import SQLALCHEMY_DB_URI
+from datetime import date
+
+# Import tables
+from db_models import Exchange, Monetary, BanksIncomesExpenses, Investment, GrossExtDebt
+from db_models import EconomicActivity, Budget, Res, Inflation
+
+def create_db(db_uri=SQLALCHEMY_DB_URI):
+    """ Create new database if it does not exist """
+    assert not database_exists(db_uri)
+    create_database(db_uri, encoding='utf8')
+
+def create_tables(db_uri = SQLALCHEMY_DB_URI):
+    engine = create_engine(db_uri, echo=True)
+
+
