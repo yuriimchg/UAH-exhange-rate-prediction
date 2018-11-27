833c16bf8238184a5c300cdc37399fb03d63c21e
yuriimchg <yurgan4@gmail.com>
2018-11-27 11:51:28 +0200

    Parse API architecture with class composition

--- nbu_parser.py
+++ nbu_parser.py
@@ -4,9 +4,10 @@ import os
 
 
 class NBUParser:
+    """ Base class for parsing bank.gov.ua API """
 
-    def __init__(self, url_base, page, date, params={}, suffix='json'):
-        self.url_base = url_base
+    def __init__(self, base, page, date, params={}, suffix='json'):
+        self.base = base
         self.page = page
         self.date = date
         self.params = params
@@ -14,7 +15,7 @@ class NBUParser:
 
     def get_url(self):
         date = f'date={self.date}'
-        base_url = os.path.join(self.url_base, self.page)
+        base_url = os.path.join(self.base, self.page)
         params_str = '&'.join([f'{k}={v}' for k, v in self.params.items()])
         return base_url + f'?{date}&{params_str}&{self.suffix}'
 
@@ -23,16 +24,12 @@ class NBUParser:
         response = requests.get(self.get_url())
         return response.json()
 
-    def __repr__(self):
-        return self.get_url()
-
 
 class ExchangeParser(NBUParser):
 
-    def __init__(self, url_base, page, date, suffix):
-        super().__init__(url_base, page, date, suffix)
-        self.date = date
-        self.page = page
+    def __init__(self, base, date):
+        super().__init__(base, 'exchange', date, {}, 'json')
+        self.url = NBUParser(self.base, self.page, self.date, self.params, self.suffix).get_url()
 
     def parse_exchange(self):
         json_data = self.get_json()
@@ -41,12 +38,11 @@ class ExchangeParser(NBUParser):
 
 class MonetaryParser(NBUParser):
 
-    def __init__(self, url_base, page, date, suffix, params):
-        super().__init__(url_base, page, date, suffix, params)
-        self.page = page
-        self.date = date
-        self.suffix = suffix
+    def __init__(self, base, date, params):
+        super().__init__(base, 'monetary', date, params, 'json')
         self.params = params
+        self.url = NBUParser(self.base, self.page, self.date, self.params, self.suffix).get_url()
+
 
     def parse_monetary(self):
         json_data = self.get_json()
@@ -55,12 +51,10 @@ class MonetaryParser(NBUParser):
 
 class BanksIncExParser(NBUParser):
 
-    def __init__(self, url_base, page, date, suffix, params):
-        super().__init__(url_base, page, date, suffix, params)
-        self.page = page
-        self.date = date
-        self.suffix = suffix
+    def __init__(self, base, date, params):
+        super().__init__(base, 'banksincexp', date, params, 'json')
         self.params = params
+        self.url = NBUParser(self.base, self.page, self.date, self.params, self.suffix).get_url()
 
     def parse_banks(self):
         json_data = self.get_json()
@@ -69,12 +63,10 @@ class BanksIncExParser(NBUParser):
 
 class InvestmentParser(NBUParser):
 
-    def __init__(self, url_base, page, date, suffix, params):
-        super().__init__(url_base, page, date, suffix, params)
-        self.page = page
-        self.date = date
-        self.suffix = suffix
+    def __init__(self, base, date, params):
+        super().__init__(base, 'interinvestpos', date, params, 'json')
         self.params = params
+        self.url = NBUParser(self.base, self.page, self.date, self.params, self.suffix).get_url()
 
     def parse_investment(self):
         json_data = self.get_json()
@@ -83,14 +75,36 @@ class InvestmentParser(NBUParser):
 
 class GrossExtDebtParser(NBUParser):
 
-    def __init__(self, url_base, page, date, suffix, params):
-        super().__init__(url_base, page, date, suffix, params)
-        self.page = page
-        self.date = date
-        self.suffix = suffix
+    def __init__(self, base, date, params):
+        super().__init__(base, 'grossextdebt', date, params, 'json')
         self.params = params
+        self.url = NBUParser(self.base, self.page, self.date, self.params, self.suffix).get_url()
 
     def parse_ged(self):
+        json_data = self.get_json()
+        print(json_data)
+
+
+class InflationParser(NBUParser):
+
+    def __init__(self, base, date, params):
+        super().__init__(base, 'inflation', date, params)
+        self.params = params
+        self.url = NBUParser(self.base, self.page, self.date, self.params, self.suffix).get_url()
+        
+    def parse_inflation(self):
+        json_data = self.get_json()
+        print(json_data)
+
+
+class EconomicActivityParser(NBUParser):
+    
+    def __init__(self, base, date, params):
+        super().__init__(base, 'economicactivity', date, params)
+        self.params = params
+        self.url = NBUParser(self.base, self.page, self.date, self.params, self.suffix).get_url()
+        
+    def parse_activity(self):
         json_data = self.get_json()
         print(json_data)
 
@@ -100,9 +114,9 @@ class GrossExtDebtParser(NBUParser):
 # TODO: Add as many classes, as required
 
 base_url = "https://bank.gov.ua/NBUStatService/v1/statdirectory"  ###########
-# print(ExchangeParser(base_url, 'exchange', '20181126', '&json').parse_exchange())  ###########
-print(NBUParser(base_url, 'grossextdebt', '200401', {'id_api':'ed'}))   ########################
-print(NBUParser(base_url, 'exchange', '200401'))        ########################################
+print(ExchangeParser(base_url, '20181126').parse_exchange())  ###########
+# print(NBUParser(base_url, 'grossextdebt', '200401', {'id_api':'ed'}))   ########################
+# print(NBUParser(base_url, 'exchange', '200401'))        ########################################
 # e = ExchangeParser('https://bank.gov.ua/NBUStatService/v1/statdirectory', 'exchange?date=20181116&json', 'joj', 'joj')
 #
 # print(e.parse_exchange())
