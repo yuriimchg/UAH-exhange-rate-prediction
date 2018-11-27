16d6fb1087f900355d7d8da04d80f883940555f0
yuriimchg <yurgan4@gmail.com>
2018-11-27 12:55:19 +0200

    Added unittests for exchange API

---
+++ tests/unit/test_exchange_parser.py
@@ -0,0 +1,14 @@
+from app.nbu_parser import ExchangeParser, base_url
+
+parser = ExchangeParser(base_url, '20110401')
+
+def test_exchange_url_ok():
+    assert parser.get_url() == 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=20110401&&json'
+
+def test_exchange_json_ok():
+    assert len(parser.get_json()) > 0
+    assert parser.get_json()[1]['r030'] == 36
+
+def test_exchange_json_keys():
+    keys = ['r030', 'txt', 'rate', 'cc', 'exchangedate']
+    assert list(parser.get_json()[3].keys()) == keys
