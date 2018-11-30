25705fe6cdf172d9fa379baab36cef452665309f
yuriimchg <yurgan4@gmail.com>
2018-11-30 17:58:27 +0200

    added some tests

--- tests/unit/test_exchange_parser.py
+++ tests/unit/test_exchange_parser.py
@@ -1,4 +1,5 @@
 from app.nbu_parser import ExchangeParser, base_url
+from datetime import datetime
 
 parser = ExchangeParser(base_url, '20110401')
 
@@ -8,6 +9,9 @@ def test_exchange_url_ok():
 def test_exchange_json_ok():
     assert len(parser.get_json()) > 0
     assert parser.get_json()[1]['r030'] == 36
+    assert parser.get_json()[2]['cc'] == 'CAD'
+
+
 
 def test_exchange_json_keys():
     keys = ['r030', 'txt', 'rate', 'cc', 'exchangedate']
