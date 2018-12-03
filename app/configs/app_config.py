751a235665cb8bf5e94a56db34088866f33ca0c6
yuriimchg <yurgan4@gmail.com>
2018-12-03 18:49:49 +0200

    fixed database uri

--- app/configs/app_config.py
+++ app/configs/app_config.py
@@ -5,7 +5,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DB_CONFIG_DICT = {'user' : 'yurii',
                   'password' : 'asreliableas12345',
                   'host' : 'localhost',
-                  'port' : '5432'}
-SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://{}:{}@{}:{}'.format(*DB_CONFIG_DICT.values())
+                  'port' : '5432',
+                  'db_name' : 'exchange_rate'
+                  }
+SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://{}:{}@{}:{}/{}'.format(*DB_CONFIG_DICT.values())
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
